2e5cb068a4f0b6de58fd8d4a7c0d755218f0ead1
David Ben <davidbensaxofonista@gmail.com>
2026-07-22 21:09:40 -0300

    v0.8 - melhoria nas rotas

--- src/controller/routes/routes.go
+++ src/controller/routes/routes.go
@@ -6,7 +6,7 @@ import (
 )
 
 func InitRoutes(r *gin.RouterGroup) {
-	r.GET("/Testing/:nickname/:senha", controller.Testing)
+	// r.GET("/Testing/:nickname/:senha")
 	r.GET("/getUserById/:userId", controller.FindUserById)
 	r.GET("/getUserByEmail/:userEmail", controller.FindUserByEmail)
 	r.POST("/createUser", controller.CreateUser)
